bf2c3418153ff53558da9e68e59408e0985540dd
Zaneeyboy <40801075+Zaneeyboy@users.noreply.github.com>
2019-11-19 09:48:39 -0400

    Functions to populate cart database
    Populate cart database when user clicks on AddItemsToCart button from shop

--- docs/shop.js
+++ docs/shop.js
@@ -1,15 +1,29 @@
-
-
+var items = db.collection("store");
 var addItemsButtons = document.getElementsByClassName("additemtocart");
-for(var x=0;x<addItemsButtons.length;x++){
-  addItemsButtons[x].addEventListener("click", function(){
-      var itemName=this.parentElement.parentElement.parentElement.parentElement.lastElementChild.firstElementChild.firstElementChild.innerHTML;
-      console.log(itemName);
+for (var x = 0; x < addItemsButtons.length; x++) {
+  addItemsButtons[x].addEventListener("click", function() {
+    var itemName = this.parentElement.parentElement.parentElement.parentElement.lastElementChild.firstElementChild.firstElementChild.innerHTML;
+    var itemPrice = this.parentElement.parentElement.parentElement.parentElement.lastElementChild.lastElementChild.lastElementChild.innerHTML;
+    itemPrice = (itemPrice.split("$"))[1];
+    var image = this.parentElement.parentElement.parentElement.firstElementChild.getAttribute("src");
+    console.log(itemName + " " + itemPrice + " " + image);
+    addToCart(itemName, itemPrice, image);
   });
 }
 
-db.collection("store").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-    });
-});
+//adds items to cart when the AddItemsToCartButton is clicked on in the store
+function addToCart(itemName, itemPrice, image) {
+  db.collection('cart').add({
+    image: image,
+    itemName: itemName,
+    price:itemPrice
+  });
+}
+
+//reads the collection from the store document on firestore
+// db.collection("store").get().then((querySnapshot) => {
+//   querySnapshot.forEach((doc) => {
+//     var names = doc.data().itemName;
+//     console.log(doc.data().itemName);
+//   });
+// });
