ae2257c42350775a5cfad51dab03f2ca1cfe5175
SRMaharaj <srmaharaj001@gmail.com>
2019-11-20 00:30:06 -0400

    Sign up adjusted

--- docs/js/auth.js
+++ docs/js/auth.js
@@ -10,25 +10,6 @@ auth.onAuthStateChanged(user => {
     }
 });
 
-//register
-const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    //get user info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
-
-    //console.log(email,password); //test
-
-    //sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        //console.log(cred); //test
-        const modal = document.querySelector('#signupModal');
-        signupForm.reset();
-    });
-});
-
 //logout
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
